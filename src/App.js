4ecba5a004a7262d9261d8ca398dd33a88226ecf
Lucas Rodrigues Dias <86005352+lucasrd2k@users.noreply.github.com>
2022-11-01 20:52:05 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = './img/logo.png'
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        br / >
+        <
+        form >
+        <
+        label > Usuário: < /label><br / >
+        <
+        input / > < br / >
+
+        <
+        label > Senha: < /label><br / >
+        <
+        input type = 'password' / > < br / >
+        <
+        a href = '#'
+        className = 'App-link' > Esqueceu a senha ? < /a> {
+            /* Usar border-radius nos inputs, 
+                    personalizar o botão com a imagem prosicionada e 
+                    tirar o sublinhado do esqueceu a senha */
+        } <
+        br / >
+        <
+        button >
+        <
+        img src = "./img/logo.png"
+        className = 'imgButton' / >
+        Continuar <
+        /button>
+
+        <
+        button >
+        <
+        img src = "./img/logo.png"
+        className = 'imgButton' / >
+        Cadastrar <
+        /button> <
+        /form>
+
+        <
+        /header>
+
+        <
+        /div>
+    );
 }
 
 export default App;
